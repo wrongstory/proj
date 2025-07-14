349e97b268bb9e2676d495f1d5769079139568ec
L <lmwoo0327@naver.com>
2025-07-14 14:29:27 +0900

    feat: 로그인 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Proj-MW</h2>
+      <h3>login feature</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
