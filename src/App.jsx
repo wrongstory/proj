91b0ca1d3263c68c19305ce99a9b1694ae2db090
L <lmwoo0327@naver.com>
2025-07-14 14:30:17 +0900

    fix: 로그인 버그 수정

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Proj-MW</h2>
-      <h3>login feature</h3>
+      <h2>login feature</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
